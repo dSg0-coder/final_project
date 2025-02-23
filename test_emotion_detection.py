0776e8da7103a575ffc6dea859e728a59d5ee861
Your Github User Name <email@example.com>
2025-02-23 07:13:18 -0500

    Fifth Commit

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,7 +2,6 @@ from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 def test_emotion_detector(self):
-    
     result_1 = emotion_detector('I am glad this happened')
     self.assertEqual(result_1['dominant_emotion'], 'joy')
     
